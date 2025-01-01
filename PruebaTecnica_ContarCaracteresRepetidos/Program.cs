2e30c8b201a306aec68b423a6cf031e75fe4ca7b
Oscar Manuel Acosta Rios <oscar96261@gmail.com>
2025-01-01 16:37:43 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTecnica_ContarCaracteresRepetidos/Program.cs
@@ -0,0 +1,30 @@
+﻿//Prueba Tecnica Contar cuantas veces de repite un caracter en una cadena
+
+Console.WriteLine("Ingrese una cadena");
+string cadena = Console.ReadLine();
+
+Console.WriteLine("Ingrese el caracter");
+string caracter = Console.ReadLine();
+
+//contador 
+int contador = 0;
+
+// Todos los strings cuentan con un metodo llamado Enumerable.Count que permite contar cuantas veces se repite un caracter en una cadena
+//Estos se pueden contar mediante un ciclo for o un ciclo foreach
+
+//Metodo 1
+/*
+foreach (var c in cadena)
+{
+	if(c == caracter[0])
+	{
+		contador++;
+	}
+
+}
+*/
+//Metodo 2
+contador = cadena.Where(c => c == caracter[0]).Count();	
+
+
+Console.WriteLine($"Se repite la letra {caracter} {contador} veces");
